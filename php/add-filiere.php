61cf67fb2fe50f1800326e0341bd7874cee9e99c
gnidehougracia <gnidehougracia@gmail.com>
2025-11-06 14:35:17 +0100

    suppression de fichiers PHP inutilisés

--- php/add-filiere.php
+++
@@ -1,9 +0,0 @@
-<?php
-include 'db.php';
-$nom = $_POST['nom'];
-$sql = "INSERT INTO filieres (nom) VALUES (?)";
-$stmt = $conn->prepare($sql);
-$stmt->bind_param("s", $nom);
-$stmt->execute();
-echo "Filière ajoutée";
-?>
--- php/add-prof.php
+++
@@ -1,9 +0,0 @@
-<?php
-include 'db.php';
-$nom = $_POST['nom'];
-$sql = "INSERT INTO professeurs (nom) VALUES (?)";
-$stmt = $conn->prepare($sql);
-$stmt->bind_param("s", $nom);
-$stmt->execute();
-echo "Professeur ajouté";
-?>
