c49e327128b8596df448f5ead6358fdf1289ee36
KPOTON Martin <kgracieux59@gmail.com>
2025-11-06 15:08:17 +0100

    Modification du fichier mot-de-passe-oublie.js

--- assets/custom_js/mot-de-passe-oublie.js
+++ assets/custom_js/mot-de-passe-oublie.js
@@ -4,14 +4,12 @@ $(document).ready(function () {
 
         let nouveauMdp = $("#nouveau_mdp").val();
 
-        // Vérification 1 : Au moins 12 caractères
         if (nouveauMdp.length < 12) {
             alert("Erreur : Le mot de passe doit contenir au moins 12 caractères !")
         }else{
             alert("Votre nouveau mot de passe a été enrégistré avec succès.")
         }
 
-        // Vérification 2 : Doit contenir des lettres ET des chiffres
         let aDesLettres = /[a-zA-Z]/.test(nouveauMdp);
         let aDesChiffres = /[0-9]/.test(nouveauMdp);
 
@@ -21,15 +19,10 @@ $(document).ready(function () {
           alert("Succès ! Votre mot de passe a été modifié avec succès.");
         }
 
-        // Si tout est bon → message de succès + simulation d'envoi
         
-        
-        // Optionnel : rediriger vers la page de connexion après 1 seconde
         setTimeout(function () {
             window.location.href = "Connexion.html";
         }, 1000);
 
-        // Ici tu pourras plus tard ajouter l'AJAX vers PHP pour sauvegarder en base
-        // $.ajax({ url: "php/update_password.php", ... });
     });
 });
